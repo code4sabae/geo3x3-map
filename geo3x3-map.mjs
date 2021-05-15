ef722b92c5744d3db28a1b9fe9a89f4b7270da90
Taisuke Fukuno <fukuno@jig.jp>
2021-05-16 08:44:15 +0900

    update url

--- geo3x3-map.mjs
+++ geo3x3-map.mjs
@@ -1,5 +1,5 @@
 import L from "https://code4sabae.github.io/leaflet-mjs/leaflet.mjs";
-import { Geo3x3 } from "https://taisukef.github.io/Geo3x3/Geo3x3.js";
+import { Geo3x3 } from "https://geo3x3.com/Geo3x3.js";
 
 class Geo3x3Map extends HTMLElement {
   constructor () {
